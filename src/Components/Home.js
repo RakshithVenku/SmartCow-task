6dc9e3f54542010abda8f4e4b6837f1ecd8d689c
Rakshith I <raksht7@gmail.com>
2021-04-12 22:02:41 +0530

    Modified Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,12 +9,15 @@ export default function Home() {
     <div>
       <Paper className="home-wrapper" elevation={7} >
       <Grid container spacing={4} >
-        <Grid item xs={12} className="title">
+        <Grid item xs={12}>
           <Typography variant="h4">SmartCow Assignment</Typography>
         </Grid>
         <Grid item  xs={12} className="btn-content">
             <Link to="/task1"><Button className="btn-primary" id="t1-btn"><AssignmentIcon/>Task 1</Button></Link>
             <Link to="/task2"><Button className="btn-primary" id="t2-btn"><AssignmentIcon/>Task 2</Button></Link>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5">@Created by Rakshith</Typography>
         </Grid>
       </Grid>
       </Paper>
